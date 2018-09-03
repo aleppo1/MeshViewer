b4fe681138f1e3a7ac6d773a5d3b91db1cfd4e14
alberto <alberto@LOCALHOST>
2018-09-03 08:54:23 +0200

    Add project files.

---
+++ WebApplication1/Handler1.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var img = context.Request.Form["File1"] ?? context.Request.QueryString["File1"];
+
+            context.Response.ContentType = "text/html";
+            context.Response.Redirect(@"view.html?File1=" + img.Split(new Char[] { '\\' }).Last());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
